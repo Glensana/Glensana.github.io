2f5b0461de0b891db21e85cd4e702bc50247438c
Glen Sanabria <gledemar99@hotmail.com>
2025-02-08 12:45:52 -0500

    Adding more pages to valentines

---
+++ python_scripts/checkbox.py
@@ -0,0 +1,29 @@
+import re
+
+def get_checkbox_status(markdown_text):
+    """
+    Extracts the status of checkboxes from markdonw text.
+
+    Args:
+        markdown_text: A string containing markdown text.
+
+    Returns:
+        A list of tuples, where each tuple contains the checkbox (True for checked, False for unchecked).
+    """
+    checkbox_regex = r"- \[( |x)\] (.*)"
+    matches = re.findall(checkbox_regex, markdown_text)
+
+    checkbox_status = []
+    for match in matches:
+        status = True if match[0] == "x" else False
+        text = match[1]
+        checkbox_status.append((text, status))
+    return checkbox_status
+
+    
+if __name__ == "__main__":
+    with open('Genesis/select_date_idea.qmd') as qmd:
+        file = qmd.read()
+        check_status = get_checkbox_status(file)
+        print(check_status)
+
